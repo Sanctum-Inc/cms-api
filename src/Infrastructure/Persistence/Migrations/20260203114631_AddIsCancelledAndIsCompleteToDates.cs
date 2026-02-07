61b98bfb2037ee7d24d9dcfa14662df82a56298b
Tshegofatsho <tshegomotlatle87@gmail.com>
2026-02-07 15:51:02 +0200

    :card_file_box: Perform database migrations

---
+++ src/Infrastructure/Persistence/Migrations/20260203114631_AddIsCancelledAndIsCompleteToDates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCancelledAndIsCompleteToDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCanceled",
+                table: "CourtCaseDates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                table: "CourtCaseDates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCanceled",
+                table: "CourtCaseDates");
+
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                table: "CourtCaseDates");
+        }
+    }
+}
