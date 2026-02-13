6f2ec08910c7dab16561c88ab4b173acbc5ab07c
Tshegofatsho <tshegomotlatle87@gmail.com>
2026-02-13 17:58:35 +0200

    :construction:

--- src/Infrastructure/Services/CourtCaseService.cs
+++ src/Infrastructure/Services/CourtCaseService.cs
@@ -4,6 +4,7 @@ using Application.Common.Interfaces.Session;
 using Application.Common.Models;
 using Application.CourtCase.Commands.Add;
 using Application.CourtCase.Commands.Update;
+using Domain.CourtCaseDates;
 using Domain.CourtCases;
 using ErrorOr;
 using MapsterMapper;
@@ -12,11 +13,13 @@ namespace Infrastructure.Services;
 
 public class CourtCaseService : BaseService<CourtCase, CourtCaseResult, AddCommand, UpdateCommand>, ICourtCaseService
 {
+    private readonly ICourtCaseRepository _courtCaseRepository;
     public CourtCaseService(
         IMapper mapper,
         ICourtCaseRepository courtCaseRepository,
         ISessionResolver sessionResolver) : base(courtCaseRepository, mapper, sessionResolver)
     {
+        _courtCaseRepository = courtCaseRepository;
     }
 
     public async Task<ErrorOr<IEnumerable<CourtCaseNumberResult>?>> GetCaseNumbers(CancellationToken cancellationToken)
@@ -31,6 +34,61 @@ public class CourtCaseService : BaseService<CourtCase, CourtCaseResult, AddComma
         var caseNumbers = entity.Value.Select(x => new CourtCaseNumberResult(x.Id.ToString(), x.CaseNumber));
 
         return caseNumbers.ToErrorOr();
+    }
+
+    public async Task<ErrorOr<CourtCaseInformationResult>> GetCourtCaseInformationById(Guid id, CancellationToken cancellationToken)
+    {
+        var courtcase = await _courtCaseRepository.GetByIdAndUserIdAsync(id, cancellationToken);
+
+        if (courtcase is null)
+        {
+            Error.NotFound("CourtCase.NotFound", "CourtCase not found.");
+        }
+
+        return new CourtCaseInformationResult()
+        {
+            CaseId = courtcase.Id,
+            CaseNumber = courtcase.CaseNumber,
+            Location = courtcase.Location,
+            Plaintiff = courtcase.Plaintiff,
+            Defendant = courtcase.Defendant,
+            CaseType = courtcase.Type,
+            CaseOutcomes = courtcase.Outcome,
+            CreatedAt = courtcase.Created,
+            LastModified = courtcase.LastModified ?? courtcase.Created,
+            Dates = courtcase.CourtCaseDates
+                .Select(x => new CourtCaseInformationQueryDatesResult()
+                {
+                    Date = x.Created,
+                    DateType = x.Type,
+                    Description = x.Description
+                })
+                .ToList(),
+            Documents = courtcase.Documents
+                .Select(x => new CourtCaseInformationQueryDocumentsResult()
+                {
+                    DateFiled = x.Created,
+                    FileType = x.ContentType,
+                    Title = x.FileName
+                })
+                .ToList(),
+            Invoices = courtcase.Invoices
+                .Select(x => new CourtCaseInformationQueryInvoicesResult()
+                {
+                    InvoiceNumber = x.InvoiceNumber,
+                    Amount = x.Items.Sum(x => x.CostPerHour * x.Hours),
+                    Status = x.Status,
+                })
+                .ToList(),
+            Lawyers = courtcase.Lawyers
+                .Select(x => new CourtCaseInformationQueryLawyersResult()
+                {
+                    Name = x.Name,
+                    Email = x.Email,
+                    MobileNumber = x.MobileNumber,
+                })
+                .ToList()
+        };
     }
 
     protected override Guid GetIdFromUpdateCommand(UpdateCommand command)
@@ -69,5 +127,14 @@ public class CourtCaseService : BaseService<CourtCase, CourtCaseResult, AddComma
         entity.Status = command.Status;
         entity.Type = command.Type;
         entity.Outcome = command.Outcome;
+    }
+
+    private static string GetNextRelevantDate(List<CourtCaseDate> courtCaseDates)
+    {
+        var mostRecentDate = courtCaseDates
+            .OrderByDescending(x => x.Date)
+            .FirstOrDefault(x => x is { IsCanceled: false, IsComplete: false });
+
+        return mostRecentDate?.Date.ToString() ?? "";
     }
 }
